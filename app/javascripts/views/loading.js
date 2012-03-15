ec807eb5a24f1a25feaf1649505bdee563252cd0
Joshua Jones <joshua@general-metrics.com>
2012-03-15 14:01:16 -0700

    Fix up loading view.

--- app/javascripts/views/loading.js
+++ app/javascripts/views/loading.js
@@ -1,7 +1,7 @@
 Radium.LoadingView = Ember.View.extend({
-  elementId: 'loading',
-  classNames: 'progress progress-info progress-striped active'.w(),
-  template: Ember.Handlebars.compile('<div class="bar" style="width: 100%;"></div>'),
+  elementId: 'loading-modal',
+  classNames: 'modal'.w(),
+  template: Ember.Handlebars.compile('<div class="modal-body"><div class="progress progress-info progress-striped active"><div class="bar" style="width: 100%;"></div></div></div>'),
   didInsertElement: function() {
     $('body').append($('<div class="modal-backdrop fade in"/>'));
   },
