7a16b627ed1ff61330c9b58439357b0738edeaa0
adman65 <me@broadcastingadam.com>
2012-03-16 13:11:33 +0100

    Making things work

--- config/development.rb
+++ config/development.rb
@@ -1,3 +1,3 @@
 Radium.configure do |rack, config|
-  rack.use FrontendServer::AddHeader, 'X-Radium-User-API-Key', config.user_api_key
+  rack.use FrontendServer::Middleware::AddHeader, 'X-Radium-User-API-Key', config.user_api_key
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,3 @@
 Radium.configure do |rack, config|
-  rack.use FrontendServer::AddHeader, 'X-Radium-Developer-API-Key', config.developer_api_key
+  rack.use FrontendServer::Middleware::AddHeader, 'X-Radium-Developer-API-Key', config.developer_api_key
 end
