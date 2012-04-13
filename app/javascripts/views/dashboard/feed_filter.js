cddc2764eb4282538db38fef0ef52b1d20ac57ec
Joshua Jones <joshua@general-metrics.com>
2012-04-13 15:37:51 -0700

    Add contacts to dashboard filter.

--- app/javascripts/views/dashboard/feed_filter.js
+++ app/javascripts/views/dashboard/feed_filter.js
@@ -8,7 +8,8 @@ Radium.DashboardFeedFilterView = Radium.FeedFilterView.extend({
     {label: "Campaigns", kind: 'campaign', addButton: true},
     {label: "Phone Calls", kind: 'phone_call', addButton: false},
     {label: "Call Lists", kind: 'call_list', addButton: true},
-    {label: "Meetings", kind: 'meeting', addButton: true}
+    {label: "Meetings", kind: 'meeting', addButton: true},
+    {label: "Contacts", kind: 'contact', addButton: true}
   ],
   itemViewClass: Ember.View.extend({
     tagName: 'li',
