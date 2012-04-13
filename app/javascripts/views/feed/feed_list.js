85842818ff0e86697d36db896cf22fb662aa44a9
Joshua Jones <joshua@general-metrics.com>
2012-04-13 15:54:53 -0700

    Add default template for feed items.

--- app/javascripts/views/feed/feed_list.js
+++ app/javascripts/views/feed/feed_list.js
@@ -1,11 +1,12 @@
 Radium.FeedListView = Ember.View.extend({
   tagName: 'td',
   layout: Ember.Handlebars.compile('{{#with content}}<strong>{{formatTime timestamp}}</strong> {{yield}}{{/with}}'),
+  defaultTemplate: 'default_activity',
   template: Ember.computed(function() {
     var kind = this.getPath('content.kind'),
         tag = this.getPath('content.tag'),
         templateName = [kind, tag].join('_'),
         template = this.templateForName(templateName, 'template');
-    return template;
+    return template || this.get('defaultTemplate');
   }).property('content.kind', 'content.tag').cacheable()
 });
