fea70243be91814bbdfd73c3f2999d9d6db32d3b
twinturbo <me@broadcastingadam.com>
2012-04-18 20:32:51 +0200

    Add logging middleware

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,17 @@
+class InspectorMiddleware
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    req = Rack::Request.new env
+    puts req.inspect
+
+    @app.call env
+  end
+end
+
 Radium.configure do |rack, config|
   rack.use FrontendServer::Middleware::AddHeader, 'X-Radium-Developer-API-Key', config.developer_api_key
+  rack.use InspectorMiddleware
 end
