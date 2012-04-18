659a3c605d217250ddfefed732d3d6fb0e88335d
Joshua Jones <joshua@general-metrics.com>
2012-04-17 23:03:40 -0700

    New feed layout and animation when new items are pushed.

--- app/javascripts/views/feed/feed_list.js
+++ app/javascripts/views/feed/feed_list.js
@@ -1,6 +1,6 @@
 Radium.FeedListView = Ember.View.extend({
-  tagName: 'td',
-  layout: Ember.Handlebars.compile('{{#with content}}<strong>{{formatTime timestamp}}</strong> {{yield}}{{/with}}'),
+  classNames: ['feed-row'],
+  layout: Ember.Handlebars.compile('<p>{{#with content}}<strong>{{formatTime timestamp}}</strong> {{yield}}{{/with}}</p>'),
   defaultTemplate: 'default_activity',
   template: Ember.computed(function() {
     var kind = this.getPath('content.kind'),
@@ -8,5 +8,15 @@ Radium.FeedListView = Ember.View.extend({
         templateName = [kind, tag].join('_'),
         template = this.templateForName(templateName, 'template');
     return template || this.get('defaultTemplate');
-  }).property('content.kind', 'content.tag').cacheable()
+  }).property('content.kind', 'content.tag').cacheable(),
+
+  didInsertElement: function() {
+    if (this.getPath('content.isNewActivity')) {
+      this.$().addClass('pushed').hide().slideDown('fast', function() {
+        $(this).removeClass('pushed');
+      });
+      // No longer considered a new activity once added to the DOM.
+      this.setPath('content.isNewActivity', false);
+    }
+  }
 });
