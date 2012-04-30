a4467050516f52f52a659ade32a2bca1a5fcd09c
Joshua Jones <joshua@general-metrics.com>
2012-04-30 02:06:38 -0700

    Add todo buttons to user/contact pages.

--- app/javascripts/views/users/user.js
+++ app/javascripts/views/users/user.js
@@ -2,6 +2,15 @@ Radium.UserPageView = Ember.View.extend(Radium.EndlessScrolling, {
   templateName: 'user',
   contentBinding: 'Radium.selectedUserController.content',
 
+  addUserTodo: function() {
+    Radium.FormManager.send('showForm', {
+      form: 'Todo',
+      id: this.getPath('content.id'),
+      type: 'user'
+    });
+    return false;
+  },
+
   // Endless scrolling properties
   feedBinding: 'content.feed',
   pageBinding: 'content.feed.page',
