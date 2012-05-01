6af4a2fd3ff8d52f2668e479a16dd1eae360979a
Joshua Jones <joshua@general-metrics.com>
2012-05-01 11:09:19 -0700

    Don't try and add meta into datastore.

--- app/javascripts/adapters/adapter.js
+++ app/javascripts/adapters/adapter.js
@@ -359,10 +359,11 @@ DS.RadiumAdapter = DS.Adapter.extend({
 
   sideload: function(store, type, json, root) {
     var sideloadedType, mappings;
-    
+
     for (var prop in json) {
       if (!json.hasOwnProperty(prop)) { continue; }
       if (prop === root) { continue; }
+      if (prop === 'meta') { continue; }
 
       sideloadedType = type.typeForAssociation(prop);
 
