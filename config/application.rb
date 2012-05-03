7fa5351f4056bf8a850993f62c355121bf10341e
twinturbo <me@broadcastingadam.com>
2012-05-03 15:21:25 -0700

    Things basically working

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,3 @@
-class InspectorMiddleware
-  def initialize(app)
-    @app = app
-  end
-
-  def call(env)
-    req = Rack::Request.new env
-    puts req.inspect
-
-    @app.call env
-  end
-end
-
 Radium.configure do |rack, config|
-  rack.use FrontendServer::Middleware::AddHeader, 'X-Radium-Developer-API-Key', config.developer_api_key
+  rack.use Iridium::Middleware::AddHeader, 'X-Radium-Developer-API-Key', config.developer_api_key
 end
--- config/development.rb
+++ config/development.rb
@@ -1,3 +1,3 @@
 Radium.configure do |rack, config|
-  rack.use FrontendServer::Middleware::AddCookie, 'user_api_key', config.user_api_key
+  rack.use Iridium::Middleware::AddCookie, 'user_api_key', config.user_api_key
 end
