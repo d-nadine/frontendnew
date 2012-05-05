029049023e0d054921c6304ad438b786755cfaba
Joshua Jones <joshua@general-metrics.com>
2012-05-05 13:10:38 -0700

    Add some animation to email form tokens add/remove.

--- app/javascripts/views/ui/token.js
+++ app/javascripts/views/ui/token.js
@@ -1,7 +1,14 @@
 Radium.TokenView = Ember.View.extend({
+  didInsertElement: function() {
+    this.$().hide().fadeIn();
+  },
   classNames: 'alert alert-info token'.w(),
   removeEmail: function(event) {
-    this.getPath('parentView.content').removeObject(this.get('item'));
+    var self = this;
+    this.$().fadeOut('fast', function() {
+      self.getPath('parentView.content').removeObject(self.get('item'));
+    });
+    return false;
     event.preventDefault();
   },
   templateName: 'token'
