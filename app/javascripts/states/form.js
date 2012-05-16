f42be517490f5b79d8338bb0a74a3c0f2c8dbc48
Joshua Jones <joshua@general-metrics.com>
2012-05-16 11:04:47 -0700

    [Delivers #29681715] Toggle form if user clicks show form action button twice.

--- app/javascripts/states/form.js
+++ app/javascripts/states/form.js
@@ -14,7 +14,8 @@ Radium.FormManager = Ember.StateManager.create({
   empty: Ember.State.create({
     showForm: function(manager, context) {
       var form = Radium[context.form + 'Form'].create({
-                  params: context
+                  params: context,
+                  formType: context.form
                 });
       if (Radium.FormContainer.get('state') !== 'inDOM') {
         Radium.FormContainer.appendTo('#form-container');
@@ -37,12 +38,15 @@ Radium.FormManager = Ember.StateManager.create({
     },
     showForm: function(manager, context) {
       var form = Radium[context.form + 'Form'].create({
-                    params: context
+                    params: context,
+                    formType: context.form
                   }),
           container = manager.getPath('rootView.childViews');
       if (context.form !== manager.get('formName')) {
         container.removeAt(0);
         container.pushObject(form);
+      } else {
+        manager.send('closeForm');
       }
     }
   })
