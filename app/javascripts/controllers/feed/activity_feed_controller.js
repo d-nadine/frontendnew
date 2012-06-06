81337567f45a4b6d1de009b6d6999c0ebd3c4edc
Paul Cowan <dagda1@scotalt.net>
2012-06-06 11:36:52 +0100

    initial test of rendering simple activity template

--- app/javascripts/controllers/feed/activity_feed_controller.js
+++ app/javascripts/controllers/feed/activity_feed_controller.js
@@ -1,10 +1,58 @@
 Radium.ActivityFeedController = Ember.ArrayProxy.extend({
   content: Ember.A(),
+  sliceStart: 0,
+  inc: 25,
+  end: function() {
+    return this.get('length') - this.get('inc');
+  }.property('content.length'),
+  _cache: Ember.A([]),
+
+  init: function(){
+    this.set('view', Ember.ContainerView.create({
+      tagName: 'div'
+    }));
+  },
+
   bootstrapLoaded: function(){
     var feed = Radium.getPath('appController.feed');
     Radium.store.loadMany(Radium.Activity, feed);
     this.set('content', Radium.store.findMany(Radium.Activity, feed.mapProperty('id').uniq()));
-  }.observes('Radium.appController.feed')
+    var self = this;
+    console.time('manualViewCreate');
+    this.get('content').forEach(function(activity, idx) {
+      var view = Ember.View.create({
+        content: activity,
+        templateName: activity.get('kind') + '_' + activity.get('tag')
+      });
+      this.get('_cache').pushObject(view);
+    }, this);
+    console.timeEnd('manualViewCreate');
+    Ember.run.next(function() {
+      self.staggeredRendering();
+    });
+  }.observes('Radium.appController.feed'),
+
+  staggeredRendering: function() {
+    var self = this,
+        view = this.get('view'),
+        itemViews = this.get('_cache'),
+        sliceStart = this.get('sliceStart'),
+        inc = this.get('inc'),
+        sliceEnd = sliceStart + inc,
+        end = this.get('end');
+
+    console.time('slide'+sliceStart);
+    var chunk = itemViews.slice(sliceStart, sliceEnd);
+    this.set('sliceStart', sliceEnd);
+    view.get('childViews').pushObjects(chunk);
+    console.timeEnd('slide'+sliceStart);
+
+    if (sliceStart < end) {
+      Ember.run.next(function() {
+        self.staggeredRendering();
+      });
+    }
+  }
 });
 
 _.emberArrayGroupBy = function(emberArray, val) {
