34d723436f7bd9a556e76b5ce802324e416be85c
Joshua Jones <joshua@general-metrics.com>
2012-06-11 21:39:52 -0700

    Add todo email, WIP as no access to nested sender object through Data.

--- app/javascripts/models/email.js
+++ app/javascripts/models/email.js
@@ -1,6 +1,11 @@
+Radium.Sender = DS.Model.extend({
+  user: DS.belongsTo('Radium.User', {key: 'user'})
+});
+
 Radium.Email = Radium.Message.extend({
   to: DS.attr('array'),
   from: DS.attr('array'),
   subject: DS.attr('string'),
-  html: DS.attr('string')
+  html: DS.attr('string'),
+  sender: DS.belongsTo('Radium.Sender', {key: 'sender'})
 });
