1a1f8f617dc8a894cd7e0013ffbb84e2ad79663f
Paul Cowan <dagda1@scotalt.net>
2012-06-14 21:26:47 +0100

    forward scrolling working...very clunky

--- app/javascripts/controllers/feed/future_feed.js
+++ app/javascripts/controllers/feed/future_feed.js
@@ -1,13 +1,26 @@
 Radium.FutureFeed = Ember.ArrayProxy.extend({
   content: Ember.A(),
   realContent: Ember.A(),
+  cursor: 0,
   
   arrayContentDidChange: function(startIdx, removeAmt, addAmt){
     if(addAmt === 0)
       return;
 
-    if(this.get('content')[startIdx].get('dateHeader')){
+    var realContent = this.get('realContent');
 
+    if(this.get('content')[startIdx].get('dateHeader')){
+      realContent.insertAt(0, this.get('content')[startIdx]);
+      this.set('cursor', 1);
+    }else if(this.get('content')[startIdx].get('activities')){
+      var index = this.get('cursor');
+      for(var i = 0; i < addAmt; i++){
+        realContent.insertAt(index, this.get('content')[i]);
+        index++;
+      }
+    }else{
+      realContent.insertAt(1, this.get('content')[startIdx]);
+      this.set('cursor', 2);
     }
   }
 });
