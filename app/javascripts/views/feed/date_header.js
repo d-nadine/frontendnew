493041679cfd854b4ff6e8e507a1ed7d61ceec38
Paul Cowan <dagda1@scotalt.net>
2012-06-15 09:25:53 +0100

    infinite scroll animation to scroll to the date header

--- app/javascripts/views/feed/date_header.js
+++ app/javascripts/views/feed/date_header.js
@@ -1,5 +1,11 @@
 Radium.DateHeaderView = Ember.View.extend({
   classNames: ['page-header', 'date-header'],
   templateName: 'date_section_header',
-  contentBinding: 'parentView.content'
+  contentBinding: 'parentView.content',
+  didInsertElement: function(){
+    var self = this;
+    $('html,body').animate({
+      scrollTop: self.$().offset().top -50
+    }, 2000);
+  }
 });
