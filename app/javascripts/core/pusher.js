966e5768c80f9026f936901db8fbc0c0112378a4
Paul Cowan <dagda1@scotalt.net>
2012-06-16 16:56:51 +0100

    Radium.Pusher update

--- app/javascripts/core/pusher.js
+++ app/javascripts/core/pusher.js
@@ -1,21 +1,23 @@
-Radium.Pusher = Ember.Object.extend({
+Radium.Pusher = Ember.Object.create({
   pusher_appkey: '41649d2cdf5ab855c8a3',
+  feedChannelEvents: ['update'],
+  streamChannelEvents: ['created', 'Updated', 'deleted'],
   init: function(){
+
     this.pusher = new Pusher(this.pusher_appkey);
 
     this.pusher.connection.bind('state_change', function(states){
       console.log(states.current);
     });
 
-    var feedChannel = this.pusher.subscribe('feed-feed_demo');
+    var feedChannel = this.pusher.subscribe('feed-' + $.cookie('user_api_key'));
 
-    feedChannel.bind_all(this.receiveAll);
+    feedChannel.bind_all(this.feedReceive);
 
-    var streamChannel = this.pusher.subscribe('stream-feed_demo');
+    var streamChannel = this.pusher.subscribe('stream-' + $.cookie('user_api_key'));
     
     streamChannel.bind_all(this.receiveAll);
   },
-  pusher: null,
   receiveAll: function(evt, data){
     var message = (typeof evt === "string") ? evt : evt.data;
 
@@ -29,6 +31,12 @@ Radium.Pusher = Ember.Object.extend({
 
   },
   feedReceive: function(evt, data){
+    if(Radium.Pusher.feedChannelEvents.indexOf(evt) === -1){
+      console.log(evt);
+      return;
+    }
+
+    debugger;
   },
   streamReceive: function(evt, data){
   }
