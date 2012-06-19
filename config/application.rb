b4d74f641e1c70995ff55745d31e8f6544a6fa87
twinturbo <me@broadcastingadam.com>
2012-06-19 12:20:16 +0200

    Updated Iridium configuration
    This changes removes the need for settings.yml. The application
    is now configured with defaults in application.rb. The proxy
    must be configured explicitly. The Radium API key (which doesn't
    change) is added in plain view.
    
    config/development.rb still works the same way. The user API key
    is still configurable via settings.yml. config/settings.yml
    is now listed in .gitignore.

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 Radium.configure do
-  middleware.use Iridium::Middleware::AddHeader, 'X-Radium-Developer-API-Key',
-    settings.developer_api_key
+  middleware.add_header 'X-Radium-Developer-API-Key', '26233b7b68290c7c7d4eec03643d0cf3e9b88ba8', :url => /^\/api/
+
+  config.proxy '/api', 'http://api.radiumcrm.com'
 end
