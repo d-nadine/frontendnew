5706f48b7a7bce1c2bc3dfab88acceda7865844a
Paul Cowan <dagda1@scotalt.net>
2012-06-25 21:37:18 +0100

    feed_controller renamed to feed_scroller

--- app/javascripts/controllers/feed/activity_feed_controller.js
+++ app/javascripts/controllers/feed/activity_feed_controller.js
@@ -1,10 +1,11 @@
 Radium.ActivityFeedController = Ember.ArrayProxy.extend(Radium.FeedScroller, {
   forwardContent: Radium.FutureFeed.create(),
-  bootStraploaded: function(){
+  feedLoaded: function(){
     this.set('previous_activity_date', Radium.getPath('appController.feed.previous_activity_date'));
     this.set('next_activity_date', Radium.getPath('appController.feed.next_activity_date'));
   }.observes('Radium.appController.feed'),
-  bootstrapLoaded: function(){
+
+  clustersLoaded: function(){
     var clusters = Radium.getPath('appController.clusters');
 
     if (clusters) {
