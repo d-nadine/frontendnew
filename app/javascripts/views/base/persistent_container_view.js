4f637ce3218fcaa57780b707af51e537675c2ca4
Paul Cowan <dagda1@scotalt.net>
2012-06-28 08:12:48 +0100

    persistent_container_view functionality disabled

--- app/javascripts/views/base/persistent_container_view.js
+++ app/javascripts/views/base/persistent_container_view.js
@@ -1,25 +1,25 @@
 Radium.PersistentContainerView= Ember.ContainerView.extend({
-  _currentViewWillChange: Ember.beforeObserver(function() {
-    var childViews = this.get('childViews'),
-        currentView = this.get('currentView');
+  // _currentViewWillChange: Ember.beforeObserver(function() {
+  //   var childViews = this.get('childViews'),
+  //       currentView = this.get('currentView');
 
-    if (currentView) {
-      //childViews.removeObject(currentView);
-    }
-  }, 'currentView'),
+  //   if (currentView) {
+  //     //childViews.removeObject(currentView);
+  //   }
+  // }, 'currentView'),
 
-  _currentViewDidChange: Ember.observer(function() {
-    var childViews = this.get('childViews'),
-        currentView = this.get('currentView');
+  // _currentViewDidChange: Ember.observer(function() {
+  //   var childViews = this.get('childViews'),
+  //       currentView = this.get('currentView');
 
-    if (currentView) {
-        currentView.set('isVisible', true);
-        childViews.filter(function(view) {
-            return view != currentView;
-        }).setEach('isVisible', false);
-        if (childViews.indexOf(currentView) === -1) {
-            childViews.pushObject(currentView);
-        }
-    }
-  }, 'currentView')
+  //   if (currentView) {
+  //       currentView.set('isVisible', true);
+  //       childViews.filter(function(view) {
+  //           return view != currentView;
+  //       }).setEach('isVisible', false);
+  //       if (childViews.indexOf(currentView) === -1) {
+  //           childViews.pushObject(currentView);
+  //       }
+  //   }
+  // }, 'currentView')
 });
