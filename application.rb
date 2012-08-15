211eabcc18ee30e070223810afdd75c79814fe1d
twinturbo <me@broadcastingadam.com>
2012-08-15 10:12:19 +0200

    Document application config

--- application.rb
+++ application.rb
@@ -1,7 +1,10 @@
 class Radium < Iridium::Application
   config.load 'minispade', 'jquery', 'handlebars', 'ember', 'ember-data'
 
+  # Proxy the API so we can update it per ENV and hide access tokens
+  # from Javscript
   config.proxy '/api', 'http://api.radiumcrm.com'
 
+  # Add our API key to every API request going through the proxy
   middleware.add_header 'X-Radium-Developer-API-Key', '26233b7b68290c7c7d4eec03643d0cf3e9b88ba8', :url => /^\/api/
 end
