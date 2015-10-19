2eb038f72ea19f28497afa8b25e4fadd934b4f0f
dagda1 <dagda1@scotalt.net>
2015-10-19 18:01:23 +0100

    disable soureMaps

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      sourceMap: false
+  }
   });
 
   // Use `app.import` to add additional libraries to the generated
