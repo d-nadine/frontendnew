5a4eae80cd5510b3f8c732185c2e56de5850584d
Yury Kaliada <fut.wrk@gmail.com>
2016-05-03 09:32:51 +0300

    fix api url
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ set :rack_env, 'production'
 set :puma_threads, [2, 4]
 set :puma_workers, 2
 
-set :api_host, 'https://productiontest.radiumcrm.com:4433'
+set :api_host, 'https://frontend.radiumcrm.com:4433'
 set :intercom_app_id, 'd5bd1654e902b81ba0f4161ea5b45bb597bfefdf'
 
 server '104.131.122.126', user: 'deployer', roles: %w{app web db sidekiq}
